7512913cb45a7e3882b814a4d996b30cc1f50e8a
Arya Karimi <devaryakrm@gmal.com>
2022-09-22 16:56:28 +0300

    Problem solved

--- exercise.js
+++ exercise.js
@@ -1,7 +1,7 @@
-let name;
+let name = "Jack";
 let surname = "Rossi";
 
-if (name === surname) {
+if (name == "Jack" && surname == "Rossi") {
   console.log("Fullname is " + name + " " + surname);
 } else {
   console.log("Full name is invalid.");
